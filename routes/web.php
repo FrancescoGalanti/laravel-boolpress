ac62acf279476282581631130d7c2ee297564895
FrancescoGalanti <galanti_francesco>
2021-02-01 15:59:02 +0100

    working

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,14 @@ Route::get('/', function () {
 });
 
 Auth::routes(['verify' => true]);
-
+/* 
 Route::get('/home', 'HomeController@index')->name('home');
+ */
+
+ Route::prefix('admin')
+        ->namespace('Admin')
+        ->name('admin.')
+        ->middleware('auth')
+        ->group(function(){
+            Route::get('/home', 'HomeController@index')->name('home');
+        });
